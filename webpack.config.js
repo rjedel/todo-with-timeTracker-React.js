25258382b15a2b64bbfcbd11bd01b19e050eb6a0
Rafal <noahh.r@gmail.com>
2021-06-24 23:05:05 +0200

    project structure

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, "build")
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    publicPath: "/build/",
+    compress: true,
+    port: 3001,
+    historyApiFallback: true,
+    open: true,
+    logLevel: 'silent',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
